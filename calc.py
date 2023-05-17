5327b9173149ee5a482715d92683c97f419074dc
Oleg <119931573+oleg14038@users.noreply.github.com>
2023-05-18 01:00:54 +0900

    Revert "edit calc.py"

--- calc.py
+++ calc.py
@@ -4,24 +4,6 @@ from subtraction import sub
 from multiplication import mult
 
 while True:
- main
-    try:
-        num, operator, num2 = get_input_data()
-        if operator == '+':
-            print('Результат операции:', add(num, num2))
-        elif operator == '-':
-            print('Результат операции:', sub(num, num2))
-        elif operator == '*':
-            print('Результат операции:', mult(num, num2))
-        else:
-            print('Ошибка! Некорректный оператор.')
-        
-    except ValueError:
-        print('Ошибка, введите правильное значение')
-
-
-
-
     # ключевое слово:
         
 #          переменная, переменная, переменная = фукнция() 
@@ -35,4 +17,3 @@ while True:
             print('Результат операции:', ())
     #ключевое слово  ValueError:
         
- main
